eeae421ec44779a55af2828fe7407f6be7e05b10
gvirus21 <gouravkumar21.dev@gmail.com>
2024-06-26 19:22:43 +0530

    wip: improving work section

--- src/app/(sections)/Work/desktop-ui.tsx
+++ src/app/(sections)/Work/desktop-ui.tsx
@@ -1,33 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { WORK_DATA } from "./work-data";
 
 const DesktopVersion = () => {
+  const [workIndex, setWorkIndex] = useState(0);
+  const [currentWork, setCurrentWork] = useState(WORK_DATA[workIndex]);
+
+  useEffect(() => {
+    setCurrentWork(WORK_DATA[workIndex]);
+  }, [workIndex]);
+
   return (
     <div className="relative hidden lg:flex flex-row justify-between min-h-[53rem] xl:min-h-0 xl:h-[40rem] mt-32 mx-auto w-10/12 xl:w-full">
       <div className="flex flex-col justify-between h-[50rem] xl:h-[40rem]">
         <div className="w-[40rem] 2xl:w-full">
           <h3 className="text-5xl xl:text-6xl font-thin">
-            PROJECT DEMO - Schedule.
+            {currentWork.title}
           </h3>
           <h4 className="text-lg md:text-xl lg:text-4xl my-4 lg:my-10 font-semibold">
-            A One line project description.
+            {currentWork.jobTitle}
           </h4>
         </div>
-        <div className="xl:absolute xl:top-[12%] 2xl:top-14 3xl:top-0 xl:right-0 h-[12rem] lg:h-[24rem] 2xl:h-[32rem] 3xl:h-[40rem] aspect-[14/9] bg-white/50 mt-10 mx-auto"></div>
+        <div className="xl:absolute xl:top-[12%] 2xl:top-14 3xl:top-0 xl:right-0 h-[12rem] lg:min-h-[24rem] 2xl:h-[32rem] 3xl:h-[40rem] aspect-[14/9] bg-white/50 my-10 mx-auto"></div>
 
-        <ul className="flex flex-col justify-between lg:h-[16rem] xl:w-[32rem] 2xl:w-[80%] 2xl:max-w-[40rem] text-base lg:text-xl list-disc lg:mt-12 lg:pl-6">
-          <li className="my-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            quod, qui molestias ullam omnis repudiandae enim amet modi
-            recusandae magnam.
-          </li>
-          <li className="my-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            quod, qui molestias ullam omnis repudiandae enim amet modi
-            recusandae magnam.
-          </li>
-          <li className="my-2">
-            Repudiandae enim amet modi recusandae magnam.
-          </li>
+        <ul className="flex flex-col justify-between xl:w-[32rem] 2xl:w-[36rem] 3xl:w-[50rem] text-base lg:text-xl list-disc lg:pl-6">
+          {currentWork.description.map((message, i) => (
+            <li key={i} className="my-2">
+              {message}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/app/(sections)/Work/index.tsx
+++ src/app/(sections)/Work/index.tsx
@@ -8,14 +8,19 @@ interface Props {
 
 export const WorkSection = ({ timeline }: Props) => {
   return (
-    <Element name="work-section">
-      <section className="relative left-[50%] -translate-x-[50%] flex justify-between lg:min-h-[80vh] w-11/12 max-w-[140rem] mt-20 lg:mt-[14rem]">
-        <h2 className="absolute left-0 lg:left-auto lg:right-0 text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
-          My Work
-        </h2>
-        <DesktopVersion />
-        <MobileVersion />
-      </section>
+    <Element
+      name="work-section"
+      className="relative lg:h-[300rem]"
+    >
+      <div className="sticky top-0 h-screen lg:mt-[5rem] lg:pt-[10rem]">
+        <section className="relative left-1/2 transform -translate-x-1/2 flex justify-between lg:min-h-[80vh] w-11/12 max-w-[140rem]">
+          <h2 className="absolute left-0 lg:left-auto lg:right-0 text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
+            My Work
+          </h2>
+          <DesktopVersion />
+          <MobileVersion />
+        </section>
+      </div>
     </Element>
   );
 };
