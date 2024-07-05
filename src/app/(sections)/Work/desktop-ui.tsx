f03c372e2c25b5231b94677a4cf7bef528e1061d
gvirus21 <gouravkumar21.dev@gmail.com>
2024-07-05 17:29:58 +0530

    added bullet points to the work section

--- src/app/(sections)/Work/desktop-ui.tsx
+++ src/app/(sections)/Work/desktop-ui.tsx
@@ -1,10 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { AnimatePresence, motion } from "framer-motion";
-import { RxDotFilled } from "react-icons/rx";
-
-import { WORK_DATA } from "./work-data";
 import { BsDot } from "react-icons/bs";
+import { WORK_DATA } from "./work-data";
 
 const images = [
   {
@@ -21,7 +19,7 @@ const images = [
   },
 ];
 
-const textLineAnimation = {
+const textAnimation = {
   initial: {
     opacity: 0,
   },
@@ -30,7 +28,7 @@ const textLineAnimation = {
     opacity: 1,
     transition: {
       delay: 0.1,
-      duration: 2,
+      duration: 1.4,
       ease: "easeOut",
     },
   },
@@ -47,17 +45,7 @@ interface Props {
 const DesktopVersion = ({ parentRef }: Props) => {
   const [scrolling, setScrolling] = useState(false);
   const [workIndex, setWorkIndex] = useState(0);
-  const [currentWork, setCurrentWork] = useState(WORK_DATA[workIndex]);
-
   const innerContainer = useRef(null);
-
-  useEffect(() => {
-    setCurrentWork(WORK_DATA[workIndex]);
-  }, [workIndex]);
-
-  useEffect(() => {
-    console.log("work index: ", workIndex);
-  }, [workIndex]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -132,10 +120,20 @@ const DesktopVersion = ({ parentRef }: Props) => {
   return (
     <div className="relative hidden lg:flex flex-row justify-between min-h-[53rem] xl:min-h-0 xl:h-[40rem] mt-32 mx-auto w-10/12 xl:w-full">
       <div className="flex flex-col h-[50rem] xl:h-[40rem]">
-        <div className="w-[40rem] 2xl:w-full">
-          <WorkTitle workIndex={workIndex} />
-          <WorkJobTitle workIndex={workIndex} />
-        </div>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={workIndex}
+            variants={textAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="w-[40rem] 2xl:w-full"
+          >
+            <WorkTitle workIndex={workIndex} />
+            <WorkJobTitle workIndex={workIndex} />
+          </motion.div>
+        </AnimatePresence>
+
         <div className="xl:absolute xl:top-[12%] 2xl:top-14 3xl:top-0 xl:right-0 h-[12rem] lg:min-h-[24rem] 2xl:h-[32rem] 3xl:h-[40rem] aspect-[14/9] bg-white/50 my-10 mx-auto overflow-hidden">
           <motion.div
             ref={innerContainer}
@@ -174,6 +172,7 @@ const DesktopVersion = ({ parentRef }: Props) => {
             </div>
           </motion.div>
         </div>
+
         <WorkDescription workIndex={workIndex} />
       </div>
     </div>
@@ -187,85 +186,43 @@ interface TextAnimationProps {
 }
 
 const WorkTitle = ({ workIndex }: TextAnimationProps) => (
-  <AnimatePresence key={"work title"} mode="wait">
+  <>
     {workIndex === 0 && (
-      <motion.h3
-        variants={textLineAnimation}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        className="text-5xl xl:text-6xl font-thin"
-      >
-        {WORK_DATA[0].title}
-      </motion.h3>
+      <h3 className="text-5xl xl:text-6xl font-thin">{WORK_DATA[0].title}</h3>
     )}
     {workIndex === 1 && (
-      <motion.h3
-        variants={textLineAnimation}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        className="text-5xl xl:text-6xl font-thin"
-      >
-        {WORK_DATA[1].title}
-      </motion.h3>
+      <h3 className="text-5xl xl:text-6xl font-thin">{WORK_DATA[1].title}</h3>
     )}
     {workIndex === 2 && (
-      <motion.h3
-        variants={textLineAnimation}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        className="text-5xl xl:text-6xl font-thin"
-      >
-        {WORK_DATA[3].title}
-      </motion.h3>
+      <h3 className="text-5xl xl:text-6xl font-thin">{WORK_DATA[3].title}</h3>
     )}
-  </AnimatePresence>
+  </>
 );
 
 const WorkJobTitle = ({ workIndex }: TextAnimationProps) => (
-  <AnimatePresence mode="wait">
+  <>
     {workIndex === 0 && (
-      <motion.h4
-        variants={textLineAnimation}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        className="text-lg md:text-xl lg:text-4xl my-4 lg:my-10 font-semibold"
-      >
+      <h4 className="text-lg md:text-xl lg:text-4xl my-4 lg:my-10 font-semibold">
         {WORK_DATA[0].jobTitle}
-      </motion.h4>
+      </h4>
     )}
     {workIndex === 1 && (
-      <motion.h4
-        variants={textLineAnimation}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        className="text-lg md:text-xl lg:text-4xl my-4 lg:my-10 font-semibold"
-      >
+      <h4 className="text-lg md:text-xl lg:text-4xl my-4 lg:my-10 font-semibold">
         {WORK_DATA[1].jobTitle}
-      </motion.h4>
+      </h4>
     )}
     {workIndex === 2 && (
-      <motion.h4
-        variants={textLineAnimation}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        className="text-lg md:text-xl lg:text-4xl my-4 lg:my-10 font-semibold"
-      >
+      <h4 className="text-lg md:text-xl lg:text-4xl my-4 lg:my-10 font-semibold">
         {WORK_DATA[3].jobTitle}
-      </motion.h4>
+      </h4>
     )}
-  </AnimatePresence>
+  </>
 );
 
 const WorkDescription = ({ workIndex }: TextAnimationProps) => (
   <AnimatePresence mode="wait">
     <motion.div
-      variants={textLineAnimation}
+      variants={textAnimation}
       initial="initial"
       animate="animate"
       exit="exit"
@@ -293,7 +250,10 @@ const DescriptionLine = ({ description }: DescriptionLineProps) => {
   return (
     <ul className="flex flex-col justify-between xl:w-[32rem] 2xl:w-[36rem] 3xl:w-[50rem] text-base lg:text-xl list-none lg:pl-2">
       {description.map((message, i) => (
-        <div key={i} className="relative flex items-center overflow-hidden my-2">
+        <div
+          key={i}
+          className="relative flex items-center overflow-hidden my-2"
+        >
           <BsDot className="h-10 w-10 mr-4 mt-1" />
           <li className="w-[97%]">{message}</li>
         </div>
