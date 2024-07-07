308b1592e50cf87bb189916f3cf87383fa564243
gvirus21 <gouravkumar21.dev@gmail.com>
2024-07-07 12:12:52 +0530

    wip: featured work responsiveness

--- src/app/(sections)/FeaturedWork/index.tsx
+++ src/app/(sections)/FeaturedWork/index.tsx
@@ -1,20 +1,15 @@
 import { Element } from "react-scroll";
+import TextReveal from "@/components/helpers/TextReveal";
 import DesktopCardsLayout from "./DesktopCardLayout";
 import MobileCardsLayout from "./MobileCardLayout";
-import TextReveal from "@/components/helpers/TextReveal";
 
-interface Props {
-  timeline: gsap.core.Timeline | null;
-}
-
-export const FeaturedWorkSection = ({ timeline }: Props) => {
+export const FeaturedWorkSection = () => {
   return (
     <Element name="featured-work-section">
-      <section className="relative left-[50%] -translate-x-[50%] flex justify-between min-h-[90vh] lg:min-h-0 lg:max-h-[75rem] xl:max-h-[85rem] 3xl:h-[100rem] 3xl:max-h-[100rem] w-11/12 max-w-[140rem] mt-[8rem] 2xl:mt-40 overflow-y-hidden">
+      <section className="relative left-[50%] -translate-x-[50%] flex justify-between lg:max-h-[80rem] xl:max-h-[80rem] 2xl:max-h-[80rem] 3xl:max-h-[100rem] lg:h-[150vh] xl:h-[160vh] 2xl:h-[150vh] 3xl:h-[200vh] w-11/12 max-w-[140rem] mt-[8rem] 2xl:mt-40">
         <h2 className="absolute left-0 w-full text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-left uppercase">
           <TextReveal>Featured Work</TextReveal>
         </h2>
-
         <DesktopCardsLayout />
         <MobileCardsLayout />
       </section>
