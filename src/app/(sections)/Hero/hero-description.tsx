f9628035053279cef6e7fabf8b53b5ed2e83433b
gvirus21 <gouravkumar21.dev@gmail.com>
2024-07-07 20:17:16 +0530

    cleanups

--- src/app/(sections)/Hero/hero-description.tsx
+++ src/app/(sections)/Hero/hero-description.tsx
@@ -20,7 +20,7 @@ export const HeroDescription = () => {
   return (
     <p
       ref={heroDescriptionRef}
-      className="sm:text-lg md:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl xs:w-[20rem] sm:w-[23rem] md:w-[24rem] xl:w-[30rem] 2xl:w-[38rem] 3xl:w-[46rem] text-center tracking-wider sm:leading-10 mt-10 sm:mt-8 md:mt-12 2xl:mt-16 3xl:mt-24 capitalize font-sendflowers opacity-0"
+      className="text-lg md:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl xs:w-[20rem] sm:w-[23rem] md:w-[24rem] xl:w-[30rem] 2xl:w-[38rem] 3xl:w-[46rem] text-center tracking-wider sm:leading-10 mt-10 sm:mt-8 md:mt-12 2xl:mt-16 3xl:mt-24 capitalize font-sendflowers opacity-0"
     >
       I help businesses build their awesome and high converging website.
     </p>
