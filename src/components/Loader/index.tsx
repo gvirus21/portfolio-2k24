4a66d4b5bdfa262c0a31fab99cca63f3677ee67e
gvirus21 <gouravkumar21.dev@gmail.com>
2024-07-12 20:23:50 +0530

    attempt: fixing loader scroll

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -31,7 +31,7 @@ const Loader = ({ timeline }: Props) => {
   return (
     <div
       ref={loaderContainerRef}
-      className="absolute inset-0 flex justify-center items-center h-screen w-screen z-[9999] opacity-0"
+      className="absolute inset-0 flex justify-center items-center h-screen w-screen z-[9999] opacity-0 overflow-hidden"
     >
       <div className="relative h-screen w-screen">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[80%] md:-translate-y-1/2 h-[15.6rem] overflow-hidden text-black">
