d57178ccad676bf8257fabea63b03a0dc3ca65e6
gvirus21 <gouravkumar21.dev@gmail.com>
2024-07-12 20:44:00 +0530

    wip

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -34,7 +34,7 @@ const Loader = ({ timeline }: Props) => {
       className="absolute inset-0 flex justify-center items-center h-screen w-screen max-h-full max-w-full z-[9999] opacity-0 overflow-hidden bg-green-500"
     >
       <div className="relative h-screen w-screen bg-red-600">
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[80%] md:-translate-y-1/2 h-[15.6rem] overflow-hidden text-black bg-purple-400">
+        {/* <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[80%] md:-translate-y-1/2 h-[15.6rem] overflow-hidden text-black bg-purple-400">
           <div
             ref={collapseWordsRef}
             className="loader h-full md:w-[8rem] lg:w-[10rem]"
@@ -49,7 +49,7 @@ const Loader = ({ timeline }: Props) => {
             </div>
             <div className="absolute bottom-0 left-0 h-[6.5rem] w-full z-50" />
           </div>
-        </div>
+        </div> */}
         {/* <NumberLoader timeline={timeline} /> */}
       </div>
     </div>
